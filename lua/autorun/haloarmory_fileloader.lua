5c3759aed29b4e9d5dcae9586446a61491554fa8
Norway174 <norway174@terrancom.net>
2025-01-10 01:28:42 +0100

    Merged Libraries into one folder

--- lua/autorun/haloarmory_fileloader.lua
+++ lua/autorun/haloarmory_fileloader.lua
@@ -5,8 +5,7 @@ HALOARMORY.Config = HALOARMORY.Config or {}
 if SERVER then resource.AddWorkshop( "2851837932" ) end
 
 local loadFolders = {
-    "haloarmory/ui3d2d", -- Library for 3D2D UIs. Used for in-world UIs.
-    "haloarmory/markdown", -- Library for converting Markdown to HTML.
+    "haloarmory/libraries", -- Libraries modules.
     --"haloarmory/character_manager", -- In active dev. Do not enable.
     "haloarmory/halo_armory", -- Adds the armory.
     --"haloarmory/halo_ai", -- Adds the ChatGPT AI interace. Disabled. Rewrite coming soon!
