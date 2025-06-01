912938cb588d0545f1b07a653cc3d171d95be4f0
Norway174 <norway174@terrancom.net>
2025-06-01 18:58:38 +0200

    Updated AR2 Helmet App

--- lua/haloarmory/halo_helmet_ar/helmet_apps/cl_ar_iff2.lua
+++ lua/haloarmory/halo_helmet_ar/helmet_apps/cl_ar_iff2.lua
@@ -11,11 +11,17 @@ local ARIFF2_HOOK = "HALOARMORY.DrawARIFF2"
 
 HALOARMORY.AR.IFF2.IFFenabled = HALOARMORY.AR.IFF2.IFFenabled or false
 
-
+local IFF2_CloseFriend = Color( 0, 255, 213)
 local IFF2_Friend = Color( 0, 255, 0 )
 local IFF2_Foe = Color( 255, 0, 0 )
 local IFF2_Neutral = Color( 255, 255, 255)
 local IFF2_Prop = Color( 255, 225, 142)
+
+local HALO_BLURX = 1
+local HALO_BLURY = 1
+local HALO_PASSES = 1
+local HALO_ADDITIVE = true
+local HALO_IGNORE_Z = false
 
 --hook.Add("PreDrawOutlines", ARIFF2_HOOK, function()
 --	HALOARMORY.AR.IFF2.DrawIFF2()
@@ -25,28 +31,54 @@ local IFF2_Prop = Color( 255, 225, 142)
 function HALOARMORY.AR.IFF2.DrawIFF2()
 	if !HALOARMORY.AR.IFF2.IFFenabled then return end
 
-	--outline.Add(ents, color, mode, render_type, outline_thickness)
+	for _, ent in ents.Iterator() do
+		if !IsValid( ent ) then continue end
 
-	local Outline_Ents = {}
+		if ( ent.DeviceName == "Crate" && ent.DeviceType == "storage" ) then
+			halo.Add( {ent}, IFF2_Prop, HALO_BLURX, HALO_BLURY, HALO_PASSES, HALO_ADDITIVE, HALO_IGNORE_Z )
 
-	// For each ents
-	for k, v in pairs( ents.GetAll() ) do
-		if IsValid( v ) then
-			
-			if isentity( v ) and v.GetModel then
-				if util.IsValidProp( tostring( v:GetModel() or "" ) ) then
-					table.insert( Outline_Ents, v )
+		elseif ( ent:IsPlayer() ) then
+			local LPly = LocalPlayer()
+			--if ent == LPly then continue end
+			if !ent:Alive() then continue end
+			if ent:GetMoveType() == MOVETYPE_NOCLIP then continue end
+
+			if ent:Team() == LPly:Team() then
+				halo.Add( {ent}, IFF2_CloseFriend, HALO_BLURX, HALO_BLURY, HALO_PASSES, HALO_ADDITIVE, HALO_IGNORE_Z )
+			else
+				halo.Add( {ent}, IFF2_Friend, HALO_BLURX, HALO_BLURY, HALO_PASSES, HALO_ADDITIVE, HALO_IGNORE_Z )
+			end
+
+		elseif ( ent:IsNPC() || ent:IsNextBot() ) then
+			if IsEnemyEntityName( ent:GetClass() ) then
+				halo.Add( {ent}, IFF2_Foe, HALO_BLURX, HALO_BLURY, HALO_PASSES, HALO_ADDITIVE, HALO_IGNORE_Z )
+			elseif IsFriendEntityName( ent:GetClass() ) then
+				halo.Add( {ent}, IFF2_Friend, HALO_BLURX, HALO_BLURY, HALO_PASSES, HALO_ADDITIVE, HALO_IGNORE_Z )
+			else
+
+				// Check if the NPC name contains spv3
+				if string.find( ent:GetClass(), "spv3" ) then
+					// It's an SPV3 NPC. So let's add special rules.
+					// Friends will contain "unsc" in the name.
+					// Hostiles will contain "cov" and "flood" in the name.
+					// We want to ignore all with "dropship" or "turret" in the name.
+
+					if string.find( ent:GetClass(), "dropship" ) || string.find( ent:GetClass(), "turret" ) then continue end
+
+					if string.find( ent:GetClass(), "unsc" ) then
+						halo.Add( {ent}, IFF2_Friend, HALO_BLURX, HALO_BLURY, HALO_PASSES, HALO_ADDITIVE, HALO_IGNORE_Z )
+					elseif string.find( ent:GetClass(), "cov" ) || string.find( ent:GetClass(), "flood" ) then
+						halo.Add( {ent}, IFF2_Foe, HALO_BLURX, HALO_BLURY, HALO_PASSES, HALO_ADDITIVE, HALO_IGNORE_Z )
+					else
+						halo.Add( {ent}, IFF2_Neutral, HALO_BLURX, HALO_BLURY, HALO_PASSES, HALO_ADDITIVE, HALO_IGNORE_Z )
+					end
+				else
+					halo.Add( {ent}, IFF2_Neutral, HALO_BLURX, HALO_BLURY, HALO_PASSES, HALO_ADDITIVE, HALO_IGNORE_Z )
 				end
 			end
-			
 		end
 	end
 
-	--outline.Add( Outline_Ents, IFF2_Foe, OUTLINE_MODE_NOTVISIBLE, OUTLINE_RENDERTYPE_AFTER_EF, 1 )
-	--print("IFF2: ", tostring(success))
-
-	halo.Add( Outline_Ents, IFF_Foe, 2, 2, 1, true, false )
-		
 end
 hook.Add("PreDrawHalos", ARIFF2_HOOK, HALOARMORY.AR.IFF2.DrawIFF2)
 
@@ -74,12 +106,12 @@ end )
 
 
 
--- HALOARMORY.AR.RegisterApp( "AR_IFF2", -1, "Toggle HUD2", "vgui/haloarmory/icons/IFF.png", function()
---     RunConsoleCommand( "AR_IFF2.Toggle" )
---     surface.PlaySound( "buttons/button24.wav" )
--- end,
--- function()
---     return HALOARMORY.AR.IFF2.IFFenabled and "IFF2 HUD [ON]" or "IFF2 HUD [OFF]"
--- end )
+HALOARMORY.AR.RegisterApp( "AR_IFF2", -1, "Toggle HUD2", "vgui/haloarmory/icons/IFF.png", function()
+    RunConsoleCommand( "AR_IFF2.Toggle" )
+    surface.PlaySound( "buttons/button24.wav" )
+end,
+function()
+    return HALOARMORY.AR.IFF2.IFFenabled and "IFF2 HUD [ON]" or "IFF2 HUD [OFF]"
+end )
 
 
